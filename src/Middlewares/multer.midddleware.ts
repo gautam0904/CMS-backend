f5bc025ca637f7da5c829f6a3a48d8beb2b7b427
gautam malaviya <103646088+gautam0904@users.noreply.github.com>
2024-12-05 18:06:01 +0530

    Update multer.midddleware.ts

--- src/Middlewares/multer.midddleware.ts
+++ src/Middlewares/multer.midddleware.ts
@@ -4,7 +4,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req: any, file: any, cb: Function) {
-    cb(null, "./public/Temp");
+    cb(null, "./public");
   },
   filename: function (reqm, file, cb) {
     cb(null, file.originalname);
